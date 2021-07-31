a50ae63a8eafa30d1402acb99c3398fdf06e64be
KazukiNagahiro <ka5zu2ki9@gmail.com>
2021-07-31 13:02:40 +0900

    [add]記事削除

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
 Route::get('/posts/{post}/edit', 'PostController@edit');
 Route::put('posts/{post}', 'PostController@update');
-
+Route::delete('/posts/{post}', 'PostController@destroy');
 Route::post('/posts', 'PostController@store');
